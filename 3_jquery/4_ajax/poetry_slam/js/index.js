f73eeac6cc334c019424dad804f3609bbdf1d437
James Wang <j.wang5441@gmail.com>
2018-09-18 18:50:44 -0400

    poetry slam solutions

--- 3_jquery/4_ajax/poetry_slam/js/index.js
+++ 3_jquery/4_ajax/poetry_slam/js/index.js
@@ -1,2 +1,9 @@
 // YOUR CODE HERE
-// URL to GET: http://horizons-json-cors.s3.amazonaws.com/poem.txt
+// URL to GET: https://gtbitsofgood.github.io/bootcamp-static/poem.txt
+$.ajax({
+  url: 'https://horizons-json-cors.s3.amazonaws.com/poem.txt',
+})
+.done( r => {
+  let count = r.split(" ").length
+  $("#count").text(count)
+})
