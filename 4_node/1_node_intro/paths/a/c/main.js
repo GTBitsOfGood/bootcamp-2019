4239836748cc60cbfe1cd410e1b85b8ff57b531c
Phildesro123 <phildesro@yahoo.com>
2018-10-01 18:41:43 -0400

    Updated main (pretty cool stuff)

--- 4_node/1_node_intro/paths/a/c/main.js
+++ 4_node/1_node_intro/paths/a/c/main.js
@@ -14,3 +14,9 @@
 //    *
 //   *
 //  *
+let five = require('./five.js')
+let fs = require('fs')
+let fileContent = fs.readFileSync('./main.js', 'utf8')
+console.log(fileContent)
+fs.writeFileSync('./main.js', 'console.log("AHHH")')
+five.logger(five.value)
