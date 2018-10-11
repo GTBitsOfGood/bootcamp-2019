68a2753261207a5469f877d4adc2349a3d9768ed
Phildesro123 <phildesro@yahoo.com>
2018-10-11 01:43:47 -0400

    Finished intro excercise

--- 6_mongo/1_mongo_intro/models.js
+++ 6_mongo/1_mongo_intro/models.js
@@ -1,5 +1,5 @@
 "use strict";
-
+require('dotenv').config();
 if (! process.env.MONGODB_URI) {
   console.error('MONGODB_URI missing, make sure you run "source env.sh"');
   process.exit(1);
@@ -7,10 +7,23 @@ if (! process.env.MONGODB_URI) {
 
 // First let's set up our MongoDb connection
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true})
 
 var Cat; // YOUR CODE HERE - define the cat model
-
+const catSchema = new mongoose.Schema({
+  name: {
+    type:String,
+    lowercase: true
+  },
+  furColor: String
+})
+Cat = mongoose.model("Cat",catSchema)
+let crookshanks = new Cat({name:"Crookshanks", furColor:"Black"})
+let bigglesworth = new Cat({name:"Mr Bigglesworth", furColor:"White"})
+let empuress = new Cat({name:"Empurress",furColor:"Calico"})
+crookshanks.save()
+bigglesworth.save()
+empuress.save()
 Cat.find(function(error, cats) {
   if (error) {
     console.log("Can't find cats", error);
