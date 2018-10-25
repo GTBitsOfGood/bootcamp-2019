4ded9dcb6c0b370fcf5165f9c941c132c3103669
Shannon <shannonke@gatech.edu>
2018-10-25 19:25:44 -0400

    finished cookie exercise!

--- 09_passport_oauth/1_cookies/skeleton/routes/index.js
+++ 09_passport_oauth/1_cookies/skeleton/routes/index.js
@@ -16,11 +16,22 @@ var generateId = function() {
 router.use(function(req, res, next) {
   // Your middleware goes here: check for a cookie, and create one if there
   // isn't one. Use generateId() to generate a unique cookie value.
+  if (req.cookies.session_id) {
+    req.session = cookieStore[req.cookies.session_id]
+  } else {
+    let id = generateId()
+    res.cookie("session_id", id);    
+    cookieStore[id] = {};
+    req.session = cookieStore[id];
+  }
+  
   next();
 });
 
 
 router.get('/', function(req, res, next) {
+
+  console.log(req.andre)
   res.setHeader('Content-Type', 'text/html');
   if (req.session && req.session.user) {
     res.write("<h1>Welcome back, " + req.session.user + "!</h1><p><a href='/logout'>Click here</a> to logout.</p>");
@@ -38,7 +49,8 @@ router.get('/login', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   // Your code here. Set the user inside the session!
-  res.cookie("username", req.body.username);
+  //res.cookie("user", req.body.username);
+  cookieStore[req.cookies.session_id].user = req.body.username;
   res.redirect('/');
 });
 
