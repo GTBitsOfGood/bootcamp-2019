6029f14d695c4640823b401b3c69bd1ca100ed8d
Michael Chen <flamingdescent@gmail.com>
2019-02-07 19:59:14 -0500

    server.js plus npm junk

--- 05_express/1_express_intro/1_poet/server.js
+++ 05_express/1_express_intro/1_poet/server.js
@@ -1,1 +1,18 @@
-// YOUR CODE HERE
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Bootcamp Poetry API')
+})
+app.get('/api/poem', (req, res) => {
+    const fs = require('fs')
+    const poem = fs.readFileSync('./poem.txt', 'uft8')
+    res.send(poem)
+})
+app.post('/api/success', (req, res) => {
+    res.json({success: true})
+})
+app.use('/api/*', (req, res) => {
+    res.send("We couldn't find any routes matching this endpoint")
+})
+app.listen(3000)
