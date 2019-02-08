4f19df7ff28b4509259c851a64b3d20ef5bf0e9b
jbaldwin2014 <jbaldwin2014@fau.edu>
2019-02-07 19:30:23 -0500

    smth

--- 04_node/1_node_intro/2_paths/a/c/main.js
+++ 04_node/1_node_intro/2_paths/a/c/main.js
@@ -4,15 +4,15 @@
 // five.js sits in the same directory so './five.js' is 
 // my relative path from main.js (current file)
 
-// var five = require('./five.js')
+// let five = require('./five.js')
 // five.logger(five.value);
 
-var one = require('./one.js');
+let one = require('./one.js');
 one.logger(one.value);
-var two = require('../a/two.js');
+let two = require('../a/two.js');
 two.logger(two.value);
-var three = require('../b/three.js');
-var four = require('./four.js');
+let three = require('../b/three.js');
+let four = require('./four.js');
 three.logger(four.value);
 
 // Output should look like
