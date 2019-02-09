a939693611d02c60d14438a54bef964d7554bd4a
Otis Smith <themroats@gmail.com>
2019-02-09 15:41:25 -0500

    express directory

--- 05_express/1_express_intro/3_handlebars_examples/3_profiles/app.js
+++ 05_express/1_express_intro/3_handlebars_examples/3_profiles/app.js
@@ -8,6 +8,22 @@ app.engine('hbs', exphbs({extname:'hbs'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.get('/', (req, res) => {
+
+    res.render('index', {filter: 'other', filtered: data});
+});
+app.get('/:filt', (req, res) => {
+    console.log(req.params.filt === "male");
+    isMale = req.params.filt.toLowerCase() === "male";
+    filtered = data.filter((item) => {
+        if (req.params.filt.toLowerCase() === "male" | req.params.filt.toLowerCase() === "female") {
+            return item["gender"].toLowerCase() === req.params.filt.toLowerCase();
+        }
+        else return true;
+    });
+    res.render('index', {filter: req.params.filt, filtered, isMale: isMale});
+});
+
 // YOUR CODE HERE
 
 app.listen(3000);
