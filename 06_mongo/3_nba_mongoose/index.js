66ccd93ccec45e6c8318a4bdd1c3aa9ea0c4ed64
Bingyao Wang <bingyaowang.nj@gmail.com>
2019-02-14 23:18:51 -0500

    mongo

--- 06_mongo/3_nba_mongoose/index.js
+++ 06_mongo/3_nba_mongoose/index.js
@@ -1,11 +1,19 @@
 // Require express and create an express app (Part 2.1)
+let express = require('express');
+let app = express();
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
 
 // Require mongoose (Part 2.2)
-
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 // Require and setup body-parser (Part 4.1)
-
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Require the Player model (Part 2.3)
-
+let Player = require('./model/player');
 // Require the Roster model (Part 5.2)
 
 // Ensure that there is a MONGODB_URI environment variable (source env.sh)
@@ -29,9 +37,30 @@ mongoose.connection.on("error", err => {
    ===================================== */
 
 // (Part 3.1)
+app.get('/', (req, res) => {
+  Player.find({}, (err, results) => {
+    if (err) console.log(err);
+    else res.send(results);
+  })
+});
 
+// Player.find({}, (err, res) => {
+//   if (err) console.log(err);
+//   return res;
+// });
 // (Part 4.2)
-
+app.post('/addPlayer', (req, res) => {
+  let newPlayer = new Player({
+      Name: req.query.name,
+      Points: req.query.points,
+      Rebounds: req.query.rebounds,
+      Assists: req.query.assists
+  });
+  newPlayer.save({}, (err, results) => {
+    if (err) res.send(err);
+    else res.send("Success");
+  })
+});
 // (Part 5.3)
 
 // (BONUS)
