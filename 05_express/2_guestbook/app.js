373504a12ed54f3f1ec6bc60dbe56a3fd86eb64b
Varshita Patakottu <varshita13@gmail.com>
2019-02-16 18:00:25 -0500

    hello

--- 05_express/2_guestbook/app.js
+++ 05_express/2_guestbook/app.js
@@ -1,20 +1,21 @@
 // This is the top level Express server application file.
-var express = require('express');
-var path = require('path');
+let express = require('express');
+let path = require('path');
 
-var app = express();
+let app = express();
 
 // Enable cookie parsing
-var cookieParser = require('cookie-parser');
+let cookieParser = require('cookie-parser');
+
 app.use(cookieParser());
 
 // Set up handlebar templates
-var exphbs = require('express-handlebars');
+let exphbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-var bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // We use this module to store and retrieve data.
 // data.read(): Read the latest data stored on disk.
 // data.save(data): Save the given data to disk.
-var data = require('./data');
+let data = require('./data');
 
 app.get('/', function(req, res) {
   res.send('Your server is working!');
@@ -41,6 +42,7 @@ app.get('/', function(req, res) {
 // For example if you wanted to render 'views/index.hbs' you'd do res.render('index')
 app.get('/login', function(req, res) {
   // YOUR CODE HERE
+  res.render('login');
 });
 
 // POST /login: Receives the form info from /login, sets a cookie on the client
@@ -51,6 +53,8 @@ app.post('/login', function(req, res) {
   res.redirect('/posts');
 });
 
+let posts= data.read()
+
 // ---Part 2. View Posts---
 
 // GET /posts: View posts page
@@ -60,12 +64,16 @@ app.post('/login', function(req, res) {
 // Hint: to get the username, use req.cookies.username
 // Hint: use data.read() to read the post data from data.json
 app.get('/posts', function (req, res) {
-  res.render('posts', {
+
+
+  let username = req.cookies.username
+
+  res.render('posts', {username, posts})
     // Pass `username` to the template from req.cookies.username
     // Pass `posts` to the template from data.read()
     // YOUR CODE HERE
+
   });
-});
 
 // ---Part 3. Create new posts---
 // GET /posts/new: Renders a form for the user to create a new form.
@@ -78,7 +86,25 @@ app.get('/posts', function (req, res) {
 // Hint: check req.cookies.username to see if user is logged in
 app.get('/posts/new', function(req, res) {
   // YOUR CODE HERE
-});
+    const name = req.cookies.username;
+    if (name) {
+      if (req.body.title == '' || req.body.date == ''|| req.body.author == '') {
+        res.sendStatus(400);
+  }   else {
+
+        res.render("post_form");
+        let newObj = {
+        author: req.body.author,
+        date: req.body.title,
+        body: req.body.date
+      }
+        posts.push(newObj);
+        data.save(posts);
+  } else {
+    res.sendStatus(401);
+
+  }
+}});
 
 // POST /posts:
 // This route is called by the form on /posts/new when a new post is being created.
@@ -95,8 +121,11 @@ app.get('/posts/new', function(req, res) {
 //
 // Read all posts with data.read(), .push() the new post to the array and
 // write it back wih data.save(array).
-app.post('/posts', function(req, res) {
+app.post('/posts/:order', function(req, res) {
   // YOUR CODE HERE
+  let ascending = req.params.order == 'ascending';
+  let backward = posts.reverse();
+  res.render('posts', {ascending, posts, backward});
 });
 
 // Start the express server
