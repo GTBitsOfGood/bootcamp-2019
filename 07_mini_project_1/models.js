63c40fbba4e95b1f65c8ee1a73bc4677b1a009c2
Bingyao Wang <bingyaowang.nj@gmail.com>
2019-02-19 22:01:20 -0500

    mini

--- 07_mini_project_1/models.js
+++ 07_mini_project_1/models.js
@@ -6,9 +6,28 @@ const mongoose = require("mongoose");
 // Project model
 const Project = mongoose.model("Project", {
   title: {
-    type: String
-  }
-  // YOUR CODE HERE
+    type: String,
+      required: true
+  },
+    goal: {
+      type: Number,
+        required: true
+    },
+    description: {
+      type: String
+    },
+    start: {
+      type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    contributions: {
+      type: []
+    }
+
 });
 
 module.exports = {
--- 07_mini_project_1/routes.js
+++ 07_mini_project_1/routes.js
@@ -23,30 +23,58 @@ router.get("/create-test-project", (req, res) => {
 // Implement the GET / endpoint.
 router.get("/", (req, res) => {
   // YOUR CODE HERE
+    Project.find((err, arr) => {
+      res.render('index', {items: arr})
+    });
 });
 
 // Part 2: Create project
 // Implement the GET /new endpoint
 router.get("/new", (req, res) => {
   // YOUR CODE HERE
+    res.render('new')
 });
 
 // Part 2: Create project
 // Implement the POST /new endpoint
 router.post("/new", (req, res) => {
   // YOUR CODE HERE
+    const newProject = new Project({
+        title: req.body.title,
+        goal: req.body.goal,
+        description: req.body.description,
+        start: strftime(req.body.start),
+        end: strftime(req.body.end)
+    });
+    newProject.save({}, (err) => {
+      if (err) res.send(err);
+      else res.redirect("/");
+    })
 });
 
 // Part 3: View single project
 // Implement the GET /project/:projectid endpoint
 router.get("/project/:projectid", (req, res) => {
   // YOUR CODE HERE
+    Project.findById(req.params.projectid, (err, pro) => {
+        res.render('project', {pro: pro})
+    })
 });
 
 // Part 4: Contribute to a project
 // Implement the GET /project/:projectid endpoint
 router.post("/project/:projectid", (req, res) => {
   // YOUR CODE HERE
+    Project.findById(req.params.projectid,(err,pro) => {
+        if (err) res.render(err);
+        pro.contributions.push({name: req.body.name,
+        amount: req.body.amount});
+        pro.save({}, (err) => {
+            res.send(err);
+        });
+        res.render('project', {pro:pro});
+    });
+
 });
 
 // Part 6: Edit project
